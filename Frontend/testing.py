e4118a95c3e1112cc4e62db7686d8367fb4a33bd
Daedagor <48215679+Daedagor@users.noreply.github.com>
2019-10-24 23:50:25 -0400

    Update testing.py

--- Frontend/testing.py
+++ Frontend/testing.py
@@ -1,10 +1,25 @@
 import unittest
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
-
-    def test_isupper(self):
+class TestEventMethods(unittest.TestCase):
+    def test_create(self):
+        test = Event.Create('test', 8.0, 0)
+        test2 = Event.Create('test2', 8.0, 1)
+        self.assertEqual(test.getName(), 'test')
+        self.assertEqual(test.getDuration(), 8.0)
+        self.assertEqual(test.getRecurring(), 0)
+        self.assertEqual(test.getCreator(),'pjh96')
+        self.assertEqual(test.getEventID(test.getName(),test.getCreator,test.getDuration(),test.getRecurring()), <eventID>)
+        
+        self.assertEqual(test.isRecurring(test.getEventID(test.getName(),test.getCreator,test.getDuration(),test.getRecurring(),test.getRecurring()), "whenisbest.com/event/<eventID>/onetime")
+        self.assertEqual(test.isRecurring(test2.getEventID(test2.getName(),test2.getCreator,test2.getDuration(),test2.getRecurring()),test2.getRecurring()), "whenisbest.com/event/<eventID>/recurring")
+        
+        self.assertEqual(test.submit(),<eventID>)
+        
+        self.assertEqual(test.share(), "whenisbest.com/event/<eventID>/share")
+                         
+    def test_attendee(self):
+                         
+                         
         self.assertTrue('FOO'.isupper())
         self.assertFalse('Foo'.isupper())
 
