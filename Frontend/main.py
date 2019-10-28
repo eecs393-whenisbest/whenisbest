e19c42f1be5ef7de87a4aae3d571588dc32e52eb
Daedagor <48215679+Daedagor@users.noreply.github.com>
2019-10-28 14:13:09 -0400

    Update main.py

--- Frontend/main.py
+++ Frontend/main.py
@@ -1,1 +1,23 @@
+#!/usr/bin/python
 from app import app
+
+
+def getName():
+  
+  return
+
+def getDuration():
+  
+  return 
+
+def getRecurring():
+  
+  return     
+
+def getCreator():
+  
+  return
+
+def getEventID(getName(), getDuration(), getRecurring(), getCreator()):
+  
+  return
