5456ce3ac789d4855f2331203a31c32beb0d6a7c
AnonMuk <zubair.mukhi@gmail.com>
2019-11-02 11:29:28 -0400

    Updates

--- Frontend/app/routes.py
+++ Frontend/app/routes.py
@@ -1,6 +1,12 @@
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
+@app.route('/main')
 def index():
     return "Hello, World!"
+
+
+def main():
+    return "aaah!"
--- Frontend/main.py
+++ Frontend/main.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python
 from app import app
 
-Schedname = "name"
+SchedName = "name"
 dur = 1.0
 recurFlag = 0
 
 def Event.create(name,duration,recuranceFlag)
-   Schedname = name
+   SchedName = name
    dur = duration
    recuranceFlag = recurFlag
 
 
 def getName():
 
-  return Schedname
+  return SchedName
 
 def getDuration():
-  
+
   return dur
 
 def getRecurring():
-  
-  return recurFlag    
+
+  return recurFlag
 
 def getCreator():
-  
+
   return
 
 def getEventID(getName(), getDuration(), getRecurring(), getCreator()):
-  
+
   return
