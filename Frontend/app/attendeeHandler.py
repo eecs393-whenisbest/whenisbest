4841883e83ff38255391ff72c239f9c35a0afb36
AnonMuk <zubair.mukhi@gmail.com>
2019-11-03 14:13:06 -0500

    uhhh

--- Frontend/app/attendeeHandler.py
+++ Frontend/app/attendeeHandler.py
@@ -1,11 +1,19 @@
 from app import sql
 
-def attendeeAccept:
+def attendeeAccept(eventID, userName, userEmail):
+    query = "insert into responses(eventID, userName, userEmail) values (%s, %s, %s)"
+    values = (eventID, userName, userEmail)
+    sql.createQuery(query, values)
 
-def getAttendeeName:
+def getAttendeeName(userEmail):
+    query = "select userName from responses where userEmail = %s"
+    values = (userEmail,)
+    return sql.getQueryResults(query, values)
 
-def getAttendeeEmail:
+def attendeeAvailability(userName):
+    query = "select userEmail from responses where userName = %s"
+    values = (userName,)
+    return sql.getQueryResults(query, values)
 
-def attendeeAvailability:
 
-def attendeeEdit
+def attendeeEdit:
