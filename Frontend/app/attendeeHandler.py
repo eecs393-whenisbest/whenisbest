bd3ae8bea3c6297bdf17454288e14606c984ca65
AnonMuk <zubair.mukhi@gmail.com>
2019-11-16 16:43:04 -0500

    Since we're using Linux now, I can't be sloppy

--- Frontend/app/attendeeHandler.py
+++ Frontend/app/attendeeHandler.py
@@ -1,17 +1,17 @@
 from app import sql
 
 def attendeeAccept(eventID, userName, userEmail):
-    query = "insert into responses(eventID, userName, userEmail) values (%s, %s, %s)"
+    query = "insert into Responses(eventID, userName, userEmail) values (%s, %s, %s)"
     values = (eventID, userName, userEmail)
     sql.createQuery(query, values)
 
 def getAttendeeName(userEmail, eventID):
-    query = "select userName from responses where userEmail = %s and eventID = %s"
+    query = "select userName from Responses where userEmail = %s and eventID = %s"
     values = (userEmail, eventID)
     return sql.getQueryResults(query, values)
 
 def attendeeAvailability(userEmail, eventID):
-    query = "select userEmail, timeSlot from responses where userName = %s and eventID = %s"
+    query = "select userEmail, timeSlot from Responses where userName = %s and eventID = %s"
     values = (userEmail, eventID)
     return sql.getQueryResults(query, values)
 
@@ -19,14 +19,14 @@ def attendeeEdit(userEmail, timeList, eventID):
     #step 1: fetch auxiliary values
     uName = getAttendeeName(userEmail, eventID)[0][0]
     #step 2: remove existing entries
-    query = "delete from responses where userEmail = %s and eventID = %s"
+    query = "delete from Responses where userEmail = %s and eventID = %s"
     values = (userEmail,eventID)
     sql.createQuery(query, values)
     #step 3: add new entries from array of times
     return attendeeSubmit(userEmail, eventID, timeSlot)
 
 def attendeeSubmit(userEmail, eventID, timeList):
-    query = "insert into responses(userEmail,eventID, timeSlot) values (%s, %s, %s)"
+    query = "insert into Responses(userEmail,eventID, timeSlot) values (%s, %s, %s)"
     for timeSlot in timeList:
         values = (userEmail, eventID, timeSlot)
         sql.createQuery(query, values)
--- Frontend/app/eventHandler.py
+++ Frontend/app/eventHandler.py
@@ -22,61 +22,61 @@ def createEvent(eName, duration, isRecurring):
     toHash = eName + str(dur) + str(recurFlag) + str(now)
     hashed = hashlib.md5(str.encode(toHash))
     eventID = hashed.hexdigest()
-    query = "insert into event(eventID, eventName, eventDuration, eventRecurs, eventShared, eventFrequency, eventCreator, eventCreationTime) values (%s, %s, %s, %s, %s, %s, %s, %s)"
+    query = "insert into Event(eventID, eventName, eventDuration, eventRecurs, eventShared, eventFrequency, eventCreator, eventCreationTime) values (%s, %s, %s, %s, %s, %s, %s, %s)"
     values = (eventID, schedName, dur, recurFlag, shared, frequency, "pjh96@case.edu", now)
     sql.createQuery(query, values)
     if (recurFlag == 1):
-        query = "insert into recurring (eventID, day, timeSlot) values (%s, %s, %s)"
+        query = "insert into Recurring (eventID, day, timeSlot) values (%s, %s, %s)"
         values = (eventID, 1, 0.25)
         sql.createQuery(query, values)
     else:
-        query = "insert into onetime (eventID, date, timeSlot) values (%s, %s, %s)"
+        query = "insert into OneTime (eventID, date, timeSlot) values (%s, %s, %s)"
         values = (eventID, date.today(), 0.25)
         sql.createQuery(query, values)
     return eventID
 
 def getName(eventID):
-    query = "select eventName from event where eventID=%s"
+    query = "select eventName from Event where eventID=%s"
     values = (eventID,)
     records = sql.getQueryResults(query, values)
     return records
 
 def getDuration(eventID):
-    query = "select eventDuration from event where eventID=%s"
+    query = "select eventDuration from Event where eventID=%s"
     values = (eventID,)
     records = sql.getQueryResults(query, values)
     return records
 
 def getRecurring(eventID):
-    query = "select eventRecurs from event where eventID=%s"
+    query = "select eventRecurs from Event where eventID=%s"
     values = (eventID,)
     records = sql.getQueryResults(query, values)
     return records
 
 def getOwner(eventID):
-    query = "select eventCreator from event where eventID=%s"
+    query = "select eventCreator from Event where eventID=%s"
     values = (eventID,)
     records = sql.getQueryResults(query, values)
     return records
 
 def getEventID(eventCreator, eventName): #TODO: formatting for datetime
-    query = "select eventID, eventCreationTime from event where eventName=%s and eventCreator=%s"
+    query = "select eventID, eventCreationTime from Event where eventName=%s and eventCreator=%s"
     values = (eventName, eventCreator)
     records = sql.getQueryResults(query, values)
     return records
 
 def deleteEventByID(eventID):
-    query = "delete from event where eventID=%s"
+    query = "delete from Event where eventID=%s"
     values = (eventID,)
     sql.createQuery(query, values)
 
 def deleteEventByCreator(eventCreator):
-    query = "delete from event where eventCreator=%s"
+    query = "delete from Event where eventCreator=%s"
     values = (eventCreator, )
     sql.createQuery(query, values)
 
 def getAllDetails(eventID):
-    query = "select * from event where eventID = %s"
+    query = "select * from Event where eventID = %s"
     values = (eventID,)
     return sql.getQueryResults(query,values)
 
