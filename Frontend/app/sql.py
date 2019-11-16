85059a5a6aa5f4d3fae3d6e5a194f2474f77d23f
AnonMuk <zubair.mukhi@gmail.com>
2019-11-16 15:58:07 -0500

    Updates for hosted app

--- Frontend/app/sql.py
+++ Frontend/app/sql.py
@@ -1,20 +1,37 @@
-import mysql.connector as mysql
-
-db = mysql.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "Shannara",
-    database = "whenisbest"
+import sqlalchemy
+import pymysql
+# The SQLAlchemy engine will help manage interactions, including automatically
+# managing a pool of connections to your database
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL(
+        drivername='mysql+pymysql',
+        username="root",
+        password="Shannara",
+        database="whenisbest",
+        query={
+            'unix_socket': '/cloudsql/{}'.format("whenisbest:us-central1:wib")
+        }
+    ),
+    pool_size = 5,
+    max_overflow = 2,
+    pool_timeout = 30,
+    pool_recycle = 1800,
+    # ... Specify additional properties here.
+    # ...
 )
 
-cursor = db.cursor()
-
 def createQuery(query, values):
-    cursor.execute(query,values)
-    db.commit()
-    print(cursor.rowcount, "record(s) modified.")
+    try:
+        with db.connect() as cursor:
+            cursor.execute(query,values)
+    except Exception as e:
+        return Response(
+            status = 500,
+            response = "error logged"
+        )
 
 def getQueryResults(query, values):
-    cursor.execute(query, values)
-    print(cursor.rowcount, "records fetched.")
-    return cursor.fetchall()
+    with db.connect() as cursor:
+        return cursor.execute(query, values).fetchall()
