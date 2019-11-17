59142ac8d37cc3d4a6ad0e7575efaa16cef1f60c
AnonMuk <zubair.mukhi@gmail.com>
2019-11-16 20:46:18 -0500

    Swaps for syntax and creation of passHandler class as well as updates to sql schema

---
+++ Frontend/app/passHandler.py
@@ -0,0 +1,31 @@
+# Following steps outlined in
+# https://nitratine.net/blog/post/how-to-hash-passwords-in-python/
+# https://www.vitoshacademy.com/hashing-passwords-in-python/
+
+import hashlib
+import os
+import binascii
+from app import sql
+
+def getHash(rawPass):
+    salt = os.urandom(32)
+    hashPass = genHash(rawPass,salt)
+    store = binascii.hexlify(salt) + binascii.hexlify(hashPass)
+    return store
+
+def confirmPass(email, guess):
+    query = "select Pass from Users where userID = %s"
+    values = (email,)
+    target = sql.getQueryResults(query,values)[0][0]
+    salt = binascii.unhexlify(target[:64])
+    currentHash = genHash(guess,salt)
+    currentTry = binascii.hexlify(salt).decode() + binascii.hexlify(currentHash).decode()
+    return currentTry == target
+
+def genHash(input, salt):
+    hashPass = hashlib.pbkdf2_hmac('sha512',
+    input.encode('utf-8'),
+    salt,
+    100000,
+    dklen=32)
+    return hashPass
--- Frontend/app/userHandler.py
+++ Frontend/app/userHandler.py
@@ -1,16 +1,34 @@
 from app import sql
 from app import emailer
+from app import passHandler
 
-def createUser():
+def createUser(email, fName, lName, rawPass):
+    hashedPass = passHandler.getHash(rawPass)
+    query = "insert into Users(userID,FName,LName,Pass) values (%s,%s,%s,%s)"
+    values = (email,fName,lName,hashedPass)
+    sql.createQuery(query,values)
 
-def editName():
+def editFirstName(fName,email):
+    query = "update Users set FName = %s where userID = %s"
+    values = (fName, email)
+    sql.createQuery(query,values)
 
-def editFirstName():
+def editLastName(lName,email):
+    query = "update Users set LName = %s where userID = %s"
+    values = (lName, email)
+    sql.createQuery(query,values)
 
-def editLastName():
+def updateEmail(email,newEmail,pwd):
+    if(passHandler.confirmPass(email,pwd)):
+        query = "update Users set userID = %s where userID = %s"
+        values = (newEmail,email)
+        sql.createQuery(query,values)
 
-def updateEmail():
+def updatePassword(email,oldPass, newPass):
+    if(passHandler.confirmPass(email,oldPass)):
+        pwd = passHandler.getHash(newPass)      
+        query = "update Users set Pass = %s where userID = %s"
+        values = (email,pwd)
+        sql.createQuery(query,values)
 
-def updatePassword():
-
-def checkPassword():
+# def checkPassword(email,pwd):
