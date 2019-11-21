6b824167d9ed2f2dc0d904f224402fe27f0a4004
Daedagor <48215679+Daedagor@users.noreply.github.com>
2019-11-21 17:34:09 -0500

    Update emailer.py

--- Frontend/app/emailer.py
+++ Frontend/app/emailer.py
@@ -1,2 +1,6 @@
 def emailEvent(address, eventID):
     print("Sending event " + eventID + " to " + address + ".")
+    return
+
+def emailRecovery(address):
+    print("Sending password link to " + address + ".")
