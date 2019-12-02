3e2f2edcc0d0b09e68222bfb96fb682fc702ef85
AnonMuk <zubair.mukhi@gmail.com>
2019-12-02 13:33:54 -0500

    Chunked Updates

--- Frontend/app/emailer.py
+++ Frontend/app/emailer.py
@@ -1,8 +1,16 @@
+from app import passHandler
+from app import sql
+
+
 def emailEvent(address, eventID):
     print("Sending event " + eventID + " to " + address + ".")
     return
 
 
 def emailRecovery(address):
-    print("Sending password link to " + address + ".")
+    key = passHandler.getHash(address)
+    query = "insert into passReset(userID, validator) values (%s, %s)"
+    values = (address, key)
+    sql.createQuery(query, values)
+    print("Sending password link to " + address + ". Key is: " + key)
     return
