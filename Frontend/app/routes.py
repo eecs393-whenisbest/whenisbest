257ed1bcb2154eae05a30703dbee061ccb105fdd
AnonMuk <zubair.mukhi@gmail.com>
2019-12-03 18:05:24 -0500

    All templates in app/templates. Web logo in app/static

--- Frontend/app/routes.py
+++ Frontend/app/routes.py
@@ -1,11 +1,15 @@
 from app import app
-from app import main as wibrunner
-from flask import Flask, render_template
+from flask import render_template
 
 
 @app.route('/')
 def home():
     return render_template('Home.html')
+
+
+@app.route('/login')
+def loginPage():
+    return render_template('Login_Page.html')
 
 
 @app.route('/main')
